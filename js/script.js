2fdee0ae2fc74667b13bbb9530e1528319ff642e
SammyCastaway <49778146+SammyCastaway@users.noreply.github.com>
2020-02-18 23:12:13 +0100

    second commit

--- js/script.js
+++ js/script.js
@@ -1,52 +1,26 @@
-document.getElementById("search-btn").addEventListener("click", calljQuery);
+document.getElementById("search-btn").addEventListener("click", callAPI);
 
+ 
 function callAPI() {
-  var f = 1;
-  var req = new XMLHttpRequest();
-  var URL = 'https://swapi.co/api/people/?page=' + f;
-  req.open('GET', URL, true);
-  req.addEventListener('load', function (){
-    if (req.status >= 200 && req.status < 400) {
-      var response = JSON.parse(req.responseText);
-      console.log(response);
 
-      for (var i = 0; i < response.results.length; i++) {
-        (function(y) {
-          var person = document.getElementById('person')
-          person.textContent = response.results[y].name;
 
-          var personHome = document.getElementById('personHome');
-          var homeReq = new XMLHttpRequest();
-          var planetURL = response.results[y].homeworld.toString();
-          homeReq.open('GET', planetURL, true);
-          homeReq.addEventListener('load', function () {
-            if (homeReq.status >= 200 && homeReq.status < 400) {
-              var planetResponse = JSON.parse(homeReq.responseText);
-              console.log(planetResponse);
-              personHome.textContent = 'Homeworld: ' + planetResponse.name;
-
-            } else {
-              console.log('Error with network request: ' + req.statusText);
-            }});
-            homeReq.send();
-        })(i);
-      }
-    } else {
-      console.log('Error with network request: ' + req.statusText);
-    }});
-    req.send();
-  
+function getRandomInt(max) {
+  return Math.floor(Math.random(87) * Math.floor(max));
 }
+let randomNum = getRandomInt(87);
 
-function calljQuery() {
-  $.ajax({url: "https://swapi.co/api/peoplex/?page=" + 1, 
-  
+
+  $.ajax({url: "https://swapi.co/api/people/" + randomNum,  
     success: function(result){
-      $("#div1").append("<ul>")
-      for (var i = 0; i < result.results.length; i++) {
-        $("#div1").append("<li>"+ result.results[i].name + "</li>");
-      }  
-      $("#div1").append("</ul>") 
+      
+      $("#person").append("Name: " + result.name);
+      $("#gender").append("Gender: " + result.gender);
+        
+        var planetURL = result.homeworld;
+        $.ajax({url: planetURL, success: function(planetresult){
+          $("#homePlanet").append("Homeplanet: " + planetresult.name);
+        }});
+        
     },
     error: function(xhr, status, error){
       var errorMessage = xhr.status + ': ' + xhr.statusText
