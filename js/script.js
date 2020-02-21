19df941d40c54351522d0f75bfdb31fce3cc8544
Samuel Björkqvist <samuel.bjorkqvist@gmail.com>
2020-02-21 16:24:35 +0100

    changes in style and added ux

--- js/script.js
+++ js/script.js
@@ -3,23 +3,31 @@ document.getElementById("search-btn").addEventListener("click", callAPI);
 
 function getRandomInt(max) {
   return Math.floor((Math.random() * max) + 1);
-}
-      
+}      
  
 function callAPI() {
+
+  $('#spinner').show();
 
   let randomNum = getRandomInt(87);
 
   $.ajax({url: "https://swapi.co/api/people/" + randomNum,  
     success: function(result){
       
+      $("#person").empty();
       $("#person").html(result.name);
-      $("#gender").html("Gender: " + result.gender);
+      const upper = result.gender.replace(/^\w/, c => c.toUpperCase());
+      $("#gender").empty();
+      $("#gender").html(upper);
         
         let planetURL = result.homeworld;
+        $("#homePlanet").empty();
         $.ajax({url: planetURL, success: function(planetresult){
-          $("#homePlanet").html("Homeplanet: " + planetresult.name);
+          $("#homePlanet").html(planetresult.name);
         }});
+        
+        $("#movieList").empty();        
+          
         if (result.films.length > 0) {
           for (var i = 0; i < result.films.length; i++) {
             
@@ -32,8 +40,10 @@ function callAPI() {
         }
         
         $('html, body').animate({
-          scrollTop: $("#info").offset().top
+          scrollTop: $("#all-info").offset().top
         }, 2000);
+
+        $('#spinner').hide();
     },
     error: function(xhr, status, error){
       let errorMessage = xhr.status + ': ' + xhr.statusText
