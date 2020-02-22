71c9d0ee993c1e96bc0d9c5cf7d3857f71557bcd
Samuel Björkqvist <samuel.bjorkqvist@gmail.com>
2020-02-22 12:27:39 +0100

    changes in style and added ux

--- js/script.js
+++ js/script.js
@@ -2,52 +2,52 @@ document.getElementById("search-btn").addEventListener("click", callAPI);
 
 
 function getRandomInt(max) {
-  return Math.floor((Math.random() * max) + 1);
-}      
- 
+    return Math.floor((Math.random() * max) + 1);
+}
+
 function callAPI() {
 
-  $('#spinner').show();
+    $('#spinner').show();
 
-  let randomNum = getRandomInt(87);
+    let randomNum = getRandomInt(87);
 
-  $.ajax({url: "https://swapi.co/api/people/" + randomNum,  
-    success: function(result){
-      
-      $("#person").empty();
-      $("#person").html(result.name);
-      const upper = result.gender.replace(/^\w/, c => c.toUpperCase());
-      $("#gender").empty();
-      $("#gender").html(upper);
-        
-        let planetURL = result.homeworld;
-        $("#homePlanet").empty();
-        $.ajax({url: planetURL, success: function(planetresult){
-          $("#homePlanet").html(planetresult.name);
-        }});
-        
-        $("#movieList").empty();        
+    $.ajax({
+        url: "https://swapi.co/api/people/" + randomNum,
+        success: function (result) {
           
-        if (result.films.length > 0) {
-          for (var i = 0; i < result.films.length; i++) {
-            
-              var movieURL = result.films[i];
-
-              $.ajax({url: movieURL, success : function(newResult){
-                $("#movieList").append("<li>" + newResult.title + "</li>");
-              }});
+          $("#person").empty();
+          $("#person").html(result.name);
+          const upper = result.gender.replace(/^\w/, c => c.toUpperCase());
+          $("#gender").empty();
+          $("#gender").html(upper);
+          let planetURL = result.homeworld;
+          $("#homePlanet").empty();
+          $.ajax({
+              url: planetURL,
+              success: function (planetresult) {
+                  $("#homePlanet").html(planetresult.name);
+              }
+          });
+          $("#movieList").empty();
+          if (result.films.length > 0) {
+              for (var i = 0; i < result.films.length; i++) {
+                  var movieURL = result.films[i];
+                  $.ajax({
+                      url: movieURL,
+                      success: function (newResult) {
+                          $("#movieList").append("<li>" + newResult.title + "</li>");
+                      }
+                  });
+              }
           }
+          $('html, body').animate({
+              scrollTop: $("#all-info").offset().top
+          }, 2000);
+          $('#spinner').hide();
+        },
+        error: function (xhr, status, error) {
+            let errorMessage = xhr.status + ': ' + xhr.statusText
+            alert('Error - ' + xhr.status);
         }
-        
-        $('html, body').animate({
-          scrollTop: $("#all-info").offset().top
-        }, 2000);
-
-        $('#spinner').hide();
-    },
-    error: function(xhr, status, error){
-      let errorMessage = xhr.status + ': ' + xhr.statusText
-      alert('Error - ' + xhr.status);
-    }
-});
+    });
 }
