7695c2959dede72b830d1e1c62d3ee5ddcac5fb8
SammyCastaway <49778146+SammyCastaway@users.noreply.github.com>
2020-02-24 00:56:40 +0100

    started testing adding more functions

--- js/script.js
+++ js/script.js
@@ -5,6 +5,66 @@ function getRandomInt(max) {
     return Math.floor((Math.random() * max) + 1);
 }
 
+
+  $(() => {
+    $("#submit").click(() =>{
+        $("#person").empty();
+        let personSearch = $("#person-search").val();
+        let personVal = document.querySelector("#people").children[0].dataset.value;
+        fetch("https://swapi.co/api/people/" + personVal,
+            {
+            })
+            .then(response => response.json())
+            .then(json => {
+                $("#person").html(json.original_title);
+                const upper = json.gender.replace(/^\w/, c => c.toUpperCase());
+                $("#gender").html(upper);
+                let planetURL = json.homeworld;
+                $("#homePlanet").empty();                
+                $("#homePlanet").html(json.homeworld);
+                $.ajax({
+                  url: planetURL,
+                  success: function (planetresult) {
+                      $("#homePlanet").html(planetresult.name);
+                  }
+              });              
+              $("#movieList").empty();
+              if (result.films.length > 0) {
+                  for (var i = 0; i < result.films.length; i++) {
+                      var movieURL = json.films[i];
+                      $.ajax({
+                          url: movieURL,
+                          success: function (newResult) {
+                              $("#movieList").append("<li>" + newResult.title + "</li>");
+            }
+    });
+
+    $("#person-search").on("input", () =>{
+        if($("#person-search").val().length > 3) {
+            let queryUrl = "https://swapi.co/api/people/partial?partial_name=" + $("#person-search").val();
+            
+            $.ajax({
+                url: queryUrl,
+                dataType: 'json',
+                success: (result) => {
+                    $("#people").empty();
+                    for(let i = 0; i < result.length; i++) {
+                        let [id, name] = result[i];
+                        $("#people").append("<option data-value='" + id + "'>" + name + "</option>");
+                    }
+                },
+                error: (xhr, status, error) => {
+                    console.log(xhr + " " + status + " " + error);
+                }
+              });
+            };
+          });
+        };
+      };
+    });
+  });
+})
+
 function callAPI() {
 
     $('#spinner').show();
